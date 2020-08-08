aeb92f720d5aef50df765f586e11fc3e29955423
jingjingliao <55671806+jingjingliao@users.noreply.github.com>
2020-08-08 09:42:59 -0700

    templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "Jingjing",
+        "title":  "Top 3 Youtube chanels for learning programming languages",
+        "content": "Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! ",
+        "date_posted": "8 August 2020"
+    },
+    {
+        "author": "Michael",
+        "title":  "Sports",
+        "content": "I love to play tennis!",
+        "date_posted": "6 August 2020"
+    }
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        "posts": posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {"title": "About"})
