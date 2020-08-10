880fd33e78a81c20da22f2183a84826cd39f4c5c
jingjingliao <55671806+jingjingliao@users.noreply.github.com>
2020-08-10 00:04:01 -0700

    user registration

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,8 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'blog.apps.BlogConfig',
     'django.contrib.admin',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -119,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
