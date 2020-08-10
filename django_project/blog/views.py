4619550adff039e03ae468de3264e6cb184ddbb0
jingjingliao <55671806+jingjingliao@users.noreply.github.com>
2020-08-09 22:52:18 -0700

    database and migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        "author": "Jingjing",
-        "title":  "Top 3 Youtube chanels for learning programming languages",
-        "content": "Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! Today is a good day! ",
-        "date_posted": "8 August 2020"
-    },
-    {
-        "author": "Michael",
-        "title":  "Sports",
-        "content": "I love to play tennis!",
-        "date_posted": "6 August 2020"
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
